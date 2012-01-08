31df13c053cb0aeabf1892e0c3670edeee461df0
Ian Wong <github+ian@squareup.com>
2012-01-08 15:43:19 -0800

    Stop spewing crap, emacs!

--- init.el
+++ init.el
@@ -29,3 +29,10 @@
 (require 'color-theme)
 (require 'color-theme-solarized)
 (color-theme-solarized-light)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(make-backup-files nil))
